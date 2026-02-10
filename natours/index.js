e390c3ea5af7633e29e9b401cb617e7569890b01
AhmeSheref <ahwy837@gmail.com>
2026-02-10 03:44:12 +0200

    handle HTTP headers

--- natours/index.js
+++ natours/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const TourRouter = require('./Routers/TourRouter')
-const UserRouter = require('./Routers/UserRouter')
+const TourRouter = require('./Routers/TourRouter');
+const UserRouter = require('./Routers/UserRouter');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
 
